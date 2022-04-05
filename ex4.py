61ba67b1c711507294ebe1e7fc25bc3f8f0a7481
Stefanneagu20 <101173798+Stefanneagu20@users.noreply.github.com>
2022-04-05 11:45:28 +0300

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,12 @@
+livres = {}
+ 
+livres["INconjurul Pamantului"] = "Jules Verne"
+livres["Singur pe mare"] = "Eu sunt autorul"
+livres["Pasagerii"] = "John Marss"
+ 
+name = "Jules Verne"
+votrename = "nome"
+while name!= votrename:
+    name = int( input("Choisissez. ") )
+else:
+    print ("")
