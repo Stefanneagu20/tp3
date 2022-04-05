7d8aecb21932aeb26a28f759718f8f3ed5865f36
Stefanneagu20 <101173798+Stefanneagu20@users.noreply.github.com>
2022-04-05 11:45:30 +0300

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,17 @@
+from operator import length_hint
+
+
+listaDeNume = ["Abricot", "Airelle", "Amande", "Ananas", "Avocat"]
+var1= "Abricot"
+var2= "Airelle"
+var3= "Amande"
+var4= "Ananas"
+var5= "Avocat"
+ 
+longueur=(len(var1))
+longueur = len(var2)
+longueur = len(var3)
+longueur = len(var4)
+longueur = len(var5)
+
+    
